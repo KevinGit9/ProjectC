9fdb193e5a74a5b222713cfa757e3300a9239b85
NourEdlbi <1028042@hr.nl>
2022-11-14 10:15:05 +0100

    idfk

--- viscon frontend/visconfrontend/src/App.tsx
+++ viscon frontend/visconfrontend/src/App.tsx
@@ -3,28 +3,11 @@ import './App.css';
 import './textbox.css';
 
 function App() {
-  return (
-      <div className="App">
-          <header className="App-header">
-              <h1>storing melden</h1>
-              <p>omschrijf het probleem zo volledig mogelijk</p>
-              
-              < text className="textbox">
-                  <text>
+    return (
 
-                      sdnjsnfjksdnfjsdnfkjhbeskhfkjdsnfkjsdnjfnsdjfnkjdsnfkjsdfkjsdjlfnslkdfjlksnflnfljsnfljdslkfjsdlkfjsldjflsdfljsdfklsflskjdflksdjflsjflksjfljsf
-
-                      sdnfkjnsdljfns
-
-                      nsdjfnljsdnfkldsfjlk
-
-                  </text>
-                  <input type="text" />
-              </text>          
-              
-              <input type="file" />
-          </header>
-      </div>
+        <div className="button">
+            <input></input>
+        </div>
   );
 }
 
