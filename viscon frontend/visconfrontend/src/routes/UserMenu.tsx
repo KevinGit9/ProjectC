cc1d5e260802db120212829bce5d66c894a58f3e
PaullGit <94172003+PaullGit@users.noreply.github.com>
2022-11-14 15:28:19 +0100

    Navigation bar 0.5
    alvast een save voor als het dalijk helemaal mis gaat :)

--- viscon frontend/visconfrontend/src/routes/UserMenu.tsx
+++ viscon frontend/visconfrontend/src/routes/UserMenu.tsx
@@ -3,11 +3,19 @@ import "./UserMenu.css";
 
 function UserMenu() {
   return (
+    <body>
+    <div className="hero">
+      <nav>
+        <img src="/visconlogo.png" className ="logo-nav" />
+        <img src="/profile-icon.png" className ="icon-profile" />
+      </nav>
+    </div>
     <div className="container">
-      <img className = "logo-1" src="/viscon-logo.jpeg" alt="image"/>
+      <img className = "logo-1" src="/small-viscon-logo.png" alt="image"/>
       <button className="btn-1"> Melding Maken </button>
       <button className="btn-2"> Mijn Meldingen </button>
     </div>
+    </body>
   );
 }
   
