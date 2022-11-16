d4fa03624039681468853d878a0de03c2965e585
NourEdlbi <1028042@hr.nl>
2022-11-16 20:40:58 +0100

    nettere styling

--- viscon frontend/visconfrontend/src/routes/admin.tsx
+++ viscon frontend/visconfrontend/src/routes/admin.tsx
@@ -4,16 +4,18 @@ import './Admin.css'
 function Admin() {
     return (
 
-        <div className = 'admin'>
+        <div className='admin'>
             <h1>Admin Name's Dashboard </h1>
+            <div className='buttons'>
             <button className='btn'>current case</button>
-            <button className='btn-1'>cases </button>
-            <button className='btn-1'>my past cases</button>
-            <Link to ="/checklist">
-                <button className='btn-1'>checklist</button>
+            <button className='btn'>cases </button>
+            <button className='btn'>my past cases</button>
+            <Link to="/checklist">
+                <button className='btn'>checklist</button>
             </Link>
-            <button className='btn-1'>add new user</button>
-            <button className='btn-1'>add new machine</button>
+            <button className='btn'>add new user</button>
+            <button className='btn'>add new machine</button>
+            </div>
         </div>
     );
     
--- viscon frontend/visconfrontend/src/routes/login.tsx
+++ viscon frontend/visconfrontend/src/routes/login.tsx
@@ -1,20 +1,30 @@
 import "./login.css"
 import { Link } from "react-router-dom";
-import './UserMenu.css'
+
 function Login() {
     return (
         <div className="loginmenu">
 
             <h1 className="heading"> Welcome to Viscon support</h1><br />
             <img className="logo-1" src="/viscon-logo.jpeg" alt="image" />
-            <input className= "btn-1" type='text'></input>
-            <input className="btn-1" type='password'></input> <br />
-            <Link to = '/machines'>
-            <input className="btn-1" type='submit'></input> <br />
-            </Link>
-            <Link to='/admin'>
-                <input className="btn-1" type='submit'></input> <br />
-            </Link>
+            <br /><br />
+            <div className='loginbutton'>
+            <h4 className = 'hh'>Log in</h4>
+            
+                Username: <br />
+                <input className="Lbtn" type='text'></input><br />
+                Password:<br/>
+                <input className="Lbtn" type='password'></input> <br />
+            
+                <Link to = '/machines'>
+                    <input className="Lbtn" type='submit'></input> 
+                </Link>
+
+                <Link to='/admin'>
+                    <input className="Lbtn" type='submit'></input> <br />
+                </Link>
+            </div>
+
             wachtwoord vergeten?
            
         </div>
