0c4d7698c0afdc3b51b3169f9e77ec6265f17e34
KevinGit9 <1037132@hr.nl>
2022-11-17 11:33:12 +0100

    linked user menu to machines

--- viscon frontend/visconfrontend/src/routes/UserMenu.tsx
+++ viscon frontend/visconfrontend/src/routes/UserMenu.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./UserMenu.css";
 
 function UserMenu() {
+  const navigate = useNavigate();
+
   return (
     <body>
     <div className="hero">
@@ -34,7 +37,7 @@ function UserMenu() {
     </div>
     <div className="container-1">
       <img className = "logo-1" src="/small-viscon-logo.png" alt="image"/>
-      <button className="btn-1"> Melding Maken </button>
+      <button className="btn-1" onClick={() => navigate("/machines")}> Melding Maken </button>
       <button className="btn-2"> Mijn Meldingen </button>
     </div>
 
