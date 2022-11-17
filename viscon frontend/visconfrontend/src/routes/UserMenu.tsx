715667fb4270f1c6ee7d02fc9a0a58e115f242fd
PaullGit <94172003+PaullGit@users.noreply.github.com>
2022-11-17 15:26:23 +0100

    Hallo

--- viscon frontend/visconfrontend/src/routes/UserMenu.tsx
+++ viscon frontend/visconfrontend/src/routes/UserMenu.tsx
@@ -1,37 +1,16 @@
 import React from 'react';
+<<<<<<< Updated upstream
+=======
+import TopBar from "../components/TopBar";
+import { useNavigate } from 'react-router-dom';
+>>>>>>> Stashed changes
 import "./UserMenu.css";
+
 
 function UserMenu() {
   return (
     <body>
-    <div className="hero">
-      <nav>
-        <img src="/visconlogo.png" className ="logo-nav" />
-        <img src="/profile-icon.png" className ="icon-profile" /> {/*onClick = {toggleMenu()}*/} 
-
-        <div className="sub-menu-wrap" id = "subMenu">
-            <div className="sub-menu">
-                <div className="user-info">
-                  <img className = "icon-profile-sub" src = "/profile-icon.png"></img>
-                  <h2> John Smith</h2>
-                </div>
-                <hr></hr>
-
-                <a href = "#" className = "sub-menu-link">
-                  <img src="images/profile.png"/>
-                  <p>View Profile Info</p>
-                  <span>{'>'}</span>
-                </a>
-                <a href = "#" className = "sub-menu-link">
-                  <img src="images/logout.png"/>
-                  <p>Logout</p>
-                  <span>{'>'}</span>
-                </a>
-   
-          </div>
-        </div>
-      </nav>
-    </div>
+      <TopBar/>
     <div className="container-1">
       <img className = "logo-1" src="/small-viscon-logo.png" alt="image"/>
       <button className="btn-1"> Melding Maken </button>
