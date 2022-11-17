feab05f8d431801a89ab4f16a9ab48a90e43245b
NourEdlbi <1028042@hr.nl>
2022-11-17 11:48:20 +0100

    Update login.tsx

--- viscon frontend/visconfrontend/src/routes/login.tsx
+++ viscon frontend/visconfrontend/src/routes/login.tsx
@@ -1,12 +1,13 @@
 import "./login.css"
 import { Link } from "react-router-dom";
+import "./UserMenu.css"
 
 function Login() {
     return (
         <div className="loginmenu">
 
             <h1 className="heading"> Welcome to Viscon support</h1><br />
-            <img className="logo-1" src="/viscon-logo.jpeg" alt="image" />
+            <img className="logo-1" src="/small-viscon-logo.png" alt="image" />
             
             <div className='loginbutton'>
             <h4 className = 'hh'>Log in</h4>
@@ -16,7 +17,7 @@ function Login() {
                 Password:<br/>
                 <input className="Lbtn" type='password'></input> <br />
             
-                <Link to = '/machines'>
+                <Link to = '/usermenu'>
                     <input className="Lbtn" type='submit'></input> 
                 </Link>
 
