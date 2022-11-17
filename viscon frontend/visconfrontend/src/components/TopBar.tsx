8aa7aad426747b52aad294182ceba82355439ba5
PaullGit <94172003+PaullGit@users.noreply.github.com>
2022-11-17 15:34:13 +0100

    Navigation bar klaar + sub profile menu

--- viscon frontend/visconfrontend/src/components/TopBar.tsx
+++ viscon frontend/visconfrontend/src/components/TopBar.tsx
@@ -1,55 +1,47 @@
 import React, { useState } from 'react';
 import "./TopBar.css";
 
-function TopBar(){
+function TopBar() {
     var subMenuWrap = "sub-menu-wrap";
     const [isOpen, setOpen] = useState(false);
+    if (isOpen) subMenuWrap = "sub-menu-wrap open-menu";
+    else subMenuWrap = "sub-menu-wrap";
+
     return (
-    <div className="hero">
-    <nav>
-      <img src="/visconlogo.png" className ="logo-nav" />
-      <button onClick = {toggleMenu}><img src="/profile-icon.png" className ="icon-profile"  /> {/*onClick = {toggleMenu()}*/}</button>
+        <div className="hero">
+            <nav>
+                <img src="/visconlogo.png" className="logo-nav" />
+                <button onClick={() => toggleMenu()}><img src="/profile-icon.png" className="icon-profile" /> {/*onClick = {toggleMenu()}*/}</button>
 
-      <div className={subMenuWrap}>
-          <div className="sub-menu">
-              <div className="user-info">
-                <img className = "icon-profile-sub" src = "/profile-icon.png"></img>
-                <h2> John Smith</h2>
-              </div>
-              <hr></hr>
+                <div className={subMenuWrap}>
+                    <div className="sub-menu">
+                        <div className="user-info">
+                            <img className="icon-profile-sub" src="/profile-icon.png"></img>
+                            <h2> John Smith</h2>
+                        </div>
+                        <hr></hr>
 
-              <a href = "#" className = "sub-menu-link">
-                <img src="images/profile.png"/>
-                <p>View Profile Info</p>
-                <span>{'>'}</span>
-              </a>
-              <a href = "#" className = "sub-menu-link">
-                <img src="images/logout.png"/>
-                <p>Logout</p>
-                <span>{'>'}</span>
-              </a>
- 
+                        <a href="#" className="sub-menu-link">
+                            <img src="images/profile.png" />
+                            <p>View Profile Info</p>
+                            <span>{'>'}</span>
+                        </a>
+                        <a href="#" className="sub-menu-link">
+                            <img src="images/logout.png" />
+                            <p>Logout</p>
+                            <span>{'>'}</span>
+                        </a>
+
+                    </div>
+                </div>
+            </nav>
         </div>
-      </div>
-    </nav>
-  </div>
     );
 
-function toggleMenu(){
-    setOpen(!isOpen);
-    if (isOpen){
-        subMenuWrap = "sub-menu-wrap open-menu";
-    }
-    else{
-        subMenuWrap = "sub-menu-wrap";
+    function toggleMenu() {
+        setOpen(!isOpen);
     }
 }
-}
 
-
-// function toggleMenu(subMenuWrap: string){
-//     subMenuWrap.concat(".open-menu");
-//     return subMenuWrap;
-// }
 
 export default TopBar;
--- viscon frontend/visconfrontend/src/routes/UserMenu.tsx
+++ viscon frontend/visconfrontend/src/routes/UserMenu.tsx
@@ -1,28 +1,22 @@
 import React from 'react';
-<<<<<<< Updated upstream
-=======
 import TopBar from "../components/TopBar";
 import { useNavigate } from 'react-router-dom';
->>>>>>> Stashed changes
 import "./UserMenu.css";
 
 
 function UserMenu() {
+  const navigate = useNavigate();
+
   return (
     <body>
       <TopBar/>
     <div className="container-1">
       <img className = "logo-1" src="/small-viscon-logo.png" alt="image"/>
-      <button className="btn-1"> Melding Maken </button>
+      <button className="btn-1" onClick={() => navigate("/machines")}> Melding Maken </button>
       <button className="btn-2"> Mijn Meldingen </button>
     </div>
 
-    {/* <script>
 
-    function toggleMenu(){
-      document.getElementById("subMenu").classList.toggle("open-menu");
-    };
-    </script> */}
     </body>
   );
 
