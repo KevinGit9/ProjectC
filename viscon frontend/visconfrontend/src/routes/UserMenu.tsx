56d28ad2cf8053f415ab006a6855da4894328d69
PaullGit <94172003+PaullGit@users.noreply.github.com>
2022-11-18 12:06:47 +0100

    Usermenu bijna af

--- viscon frontend/visconfrontend/src/routes/UserMenu.tsx
+++ viscon frontend/visconfrontend/src/routes/UserMenu.tsx
@@ -3,36 +3,39 @@ import TopBar from "../components/TopBar";
 import { useNavigate } from 'react-router-dom';
 import "./UserMenu.css";
 
-
 function UserMenu() {
   const navigate = useNavigate();
 
   return (
     <div>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet"></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet"/>
       </head>
-          <body>
-            <TopBar /> {/* van links naar rechts werken met div boxes*/}
-            <div className="circle"/>
-            <div className="circle-2"/>
-            <div className="circle-3"/>
-            <div className="circle-4"/>
+      <body>
+        <TopBar /> {/* van links naar rechts werken met div boxes*/}
+        <div className="circle" />
+        <div className="circle-2" />
+        <div className="circle-3" />
+        <div className="circle-4" />
 
-            <div className="masterContainer">
-              <div className="textBox">
-                  <h2>Let's Solve<br/><span>Together</span></h2>
-               </div>
-              <div className="mainButtonContainer">
-                {/* <img className="logo-1" src="/small-viscon-logo.png" alt="image" /> */}
-                <button className="btn-1" onClick={() => navigate("/machines")}> Create Ticket </button>
-                <button className="btn-2"> My Tickets </button>
-              </div>
+        <div className="masterContainer">
+          <div className="textBox">
+            <h2>Together<br /><span>We Solve!</span></h2>
+            <p>Help us find a solution to the problem of your device. <br />Get <span>started</span> with us</p>
+            <div className="mainButtonContainer">
+              <button className="btn-1" onClick={() => navigate("/machines")}> Create Ticket </button>
+              <button className="btn-2"> View My Tickets </button>
             </div>
-          </body>
+          <div className="VectorArt"></div>
+          </div>
         </div>
-        );
+      </body>
+    </div>
+  );
 }
 
-        export default UserMenu;
+export default UserMenu;
