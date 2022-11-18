75674e5203daa44e34997c70733c5f16132180b6
PaullGit <94172003+PaullGit@users.noreply.github.com>
2022-11-18 16:32:54 +0100

    CodeCleaning

--- viscon frontend/visconfrontend/src/routes/UserMenu.tsx
+++ viscon frontend/visconfrontend/src/routes/UserMenu.tsx
@@ -9,28 +9,27 @@ function UserMenu() {
   return (
     <div>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet"/>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet" />
       </head>
       <body>
         <TopBar /> {/* van links naar rechts werken met div boxes*/}
         <div className="circle" />
-        <div className="circle-2" />
-        <div className="circle-3" />
-        <div className="circle-4" />
-
         <div className="masterContainer">
           <div className="textBox">
-            <h2>Together<br /><span>We Solve!</span></h2>
+            <div className = "motto">
+              <h2>Together<br /><span>We Solve</span></h2>
+              <h2 className='exclamationMark'>!</h2>
+            </div>
             <p>Help us find a solution to the problem of your device. <br />Get <span>started</span> with us</p>
             <div className="mainButtonContainer">
               <button className="btn-1" onClick={() => navigate("/machines")}> Create Ticket </button>
               <button className="btn-2"> View My Tickets </button>
             </div>
-          <div className="VectorArt"></div>
+            <div className="VectorArt"></div>
           </div>
         </div>
       </body>
