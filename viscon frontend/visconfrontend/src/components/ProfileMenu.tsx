60a72abbc04e1bf23d2a2dd7d64ab4805a0820af
PaullGit <94172003+PaullGit@users.noreply.github.com>
2022-11-20 22:45:45 +0100

    (Nog niet af)Nieuwe Profile Menu

--- viscon frontend/visconfrontend/src/components/ProfileMenu.tsx
+++ viscon frontend/visconfrontend/src/components/ProfileMenu.tsx
@@ -1,35 +1,56 @@
-import React from 'react';
+import { joinPaths } from '@remix-run/router';
+import React, {useState, useEffect, useRef} from 'react';
+import { isPropertySignature } from 'typescript';
 import "./ProfileMenu.css";
 
 function ProfileMenu() {
-    return (
-        <div className="ProfileMenuContainer">
-            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-            <div className="menuContainer">
-                <div className="menuTrigger">
-                    <img src="profile-icon.png" />
-                </div>
-                <div className="dropdownMenu">
-                    <h3>The kiet<br/><span>Webdesigner</span></h3>
-                    <ul>
-                        <DropdownItem icon = {"person"} text = {"My Profile"}/>
-                        <DropdownItem icon = {"logout"} text = {"Logout"}/>
 
-                        {/* <DropdownItem icon = {""}/> */}
-                    </ul>
-                </div>
+  const [open, setOpen] = useState(false);
 
-            </div>
+  let menuRef = useRef();
+
+  useEffect(() => {
+    let handler = (e:any)=>{
+      if(!menuRef.current.contains(e.target)){
+        setOpen(false);
+        console.log(menuRef.current);
+      }      
+    };
+
+    document.addEventListener("mousedown", handler);
+    
+
+    return() =>{
+      document.removeEventListener("mousedown", handler);
+    }
+
+  });
+
+  return (
+    <div className="profileMenu">
+      <div className='menu-container' ref={menuRef}>
+        <div className='menu-trigger' onClick={()=>{setOpen(!open)}}>
+          <img src="profile-icon.png"></img>
         </div>
-    );
+
+        <div className={`dropdown-menu ${open? 'active' : 'inactive'}`} >
+          <h3>The Kiet<br/><span>Website Designer</span></h3>
+          <ul>
+            <DropdownItem i = {"person"} text = {"My Profile"}/>
+            <DropdownItem i = {"lotgout"} text = {"Logout"}/>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 function DropdownItem(props:any){
-    return(
-        <li className = "dropdownItem">
-            <span className = "material-icons">{props.icon}</span>
-            <a>{props.text}</a>
-        </li>
-    );
+  return(
+    <li className = 'dropdownItem'>
+      <i>{props.icon}</i>
+      <a>{props.text} </a>
+    </li>
+  );
 }
 export default ProfileMenu;
