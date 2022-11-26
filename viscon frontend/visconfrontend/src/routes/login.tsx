377224443b12d02350ec577243d1aeda7e38d9b0
NourEdlbi <1028042@hr.nl>
2022-11-26 13:26:11 +0100

    jjjj

--- viscon frontend/visconfrontend/src/routes/login.tsx
+++ viscon frontend/visconfrontend/src/routes/login.tsx
@@ -9,10 +9,12 @@ function Login() {
             <img className="logo-1" src="/small-viscon-logo.png" alt="image" />
             <h1 className="heading"> Welcome to Viscon support</h1>
             <div className='loginbutton'>
-            <h4 className = 'hh'>Log in</h4>
-           
-                <input placeholder='Username' className="Lbtn" type='text'></input><br />
-                <input placeholder= 'Password' className="Lbtn" type='password'></input> <br />
+                <h4 className='hh'>Log in</h4>
+                <form>
+                    <input placeholder='Username' className="Lbtn" type='text'></input><br />
+                    <input placeholder='Password' className="Lbtn" type='password'></input> <br />
+                </form>
+                
             
                 <Link to = '/usermenu'>
                     <input className="Lbtn" type='submit'></input> 
