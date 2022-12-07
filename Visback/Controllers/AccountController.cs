34e853dda42d9cdbce4d12d22411ab7b7e463ec8
NourEdlbi <1028042@hr.nl>
2022-12-07 14:21:25 +0100

    next step fetch request

---
+++ Visback/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Visback.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public static List<Account> accounts = new List<Account>
+        {
+            new Account
+            {
+                Name = "admin",
+                Password = "admin"
+            },
+            new Account
+            {
+                Name ="123",
+                Password = "123"
+            }
+        };
+        [HttpGet]
+        public async Task<ActionResult<List<string>>> getuserlist()
+        {
+            List<string> usernames = new List<string>();
+
+            foreach (var acc in accounts)
+            {
+                usernames.Add(acc.Name);
+            }
+
+            return Ok(usernames);
+        }
+        public async Task<ActionResult<Account>>LogintoAccount()
+        {           
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+            Account account = new Account();
+            account.Name = username;
+            account.Password = password;
+
+            foreach (var acc in accounts)
+            {
+                if (acc.Password == account.Password && acc.Name == account.Name)
+                {
+                    return Ok(account);
+                }
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Account>> RegisterAccount(Account account)
+        {
+            
+            accounts.Add(account);
+            return Ok(account);
+        }
+    }
+}
--- viscon frontend/visconfrontend/src/routes/login.tsx
+++ viscon frontend/visconfrontend/src/routes/login.tsx
@@ -3,25 +3,88 @@ import { Link } from "react-router-dom";
 import "./UserMenu.css";
 import React from 'react'; 
 
-function Login(){
-    return (
-        <div className="loginmenu">
-            <img className="logo-1" src="/small-viscon-logo.png" alt="image" />
-            <h1 className="heading"> Welcome to Viscon support</h1>
-            <div className='loginbutton'>
-                <h4 className='hh'>Log in</h4>
+class Login extends React.Component<any,any> {
+    constructor(props) {
+        super(props);
+        this.state = { Username:"", Password:"" };
 
-                <form onSubmit={loginsubmit}>
-                    <input placeholder='Username' className="Lbtn" type='text'></input><br />
-                    <input placeholder='Password' className="Lbtn" type='password' ></input> <br />
-                </form>
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const value = event.target.value;
+        const name = event.target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.Username + this.state.Password);
+        //fetch post request komt hier
+
+
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="loginmenu">
+                <img className="logo-1" src="/small-viscon-logo.png" alt="image" />
+                <h1 className="heading"> Welcome to Viscon support</h1>
+                <div className='loginbutton'>
+                    <h4 className='hh'>Log in</h4>
+                    <form onSubmit={this.handleSubmit}>
+                        <input name="Username" placeholder='Username' value={this.state.Username} onChange={this.handleInputChange} className="Lbtn" type='text'></input><br />
+                        <input name="Password" placeholder='Password' value={this.state.Password} onChange={this.handleInputChange} className="Lbtn" type='password' ></input> <br />
+                        <button type="submit">Log in</button>
+                    </form>
                 <a href=''>wachtwoord vergeten?</a>
             </div>
         </div>
-    )
+        );
+    }
 }
 
-const loginsubmit = ( 
-);
+//const Login = () => {
+//    const [Username, setUsername] = React.useState('');
+//    const [Password, setPassword] = React.useState('');
+
+//    const loginSubmit = event => {
+//        console.log('handleSubmit ran');
+//        event.preventDefault(); //  prevent page refresh
+
+//        //  access input values here
+//        console.log('firstName ', Username);
+//        console.log('lastName ', Password);
+
+//        //  clear all input values in the form
+//        setUsername('');
+//        setPassword('');
+//    };
+
+//    return (
+//        <div className="loginmenu">
+//            <img className="logo-1" src="/small-viscon-logo.png" alt="image" />
+//            <h1 className="heading"> Welcome to Viscon support</h1>
+//            <div className='loginbutton'>
+//                <h4 className='hh'>Log in</h4>
+
+
+//                <form onSubmit={loginSubmit}>
+//                    <input id="Username" placeholder='Username' className="Lbtn" type='text'></input><br />
+//                    <input id="Password" placeholder='Password' className="Lbtn" type='password' ></input> <br />
+//                    <button type="submit">Log in</button>
+//                </form>
+
+//                <a href=''>wachtwoord vergeten?</a>
+//            </div>
+//        </div>
+//    )
+//}
+
 
 export default Login;
