72b8e55c92b56be98d861570619326b0454ff7ca
Rayanmrf <r.elmaaroufi@hotmail.com>
2022-12-15 13:10:59 -0200

    zoo

--- viscon frontend/visconfrontend/src/routes/AdminRegistratie.tsx
+++ viscon frontend/visconfrontend/src/routes/AdminRegistratie.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './AdminRegistratie.css';
 const RegistrationLayout = () => {
   return (
     <div className="registration-layout">
--- viscon frontend/visconfrontend/src/routes/login.tsx
+++ viscon frontend/visconfrontend/src/routes/login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './login.css';
 
 const LoginLayout = () => {
   return (
