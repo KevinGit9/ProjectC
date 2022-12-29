b1a57c28dda649077522d3ac2040af00720c3035
PaullGit <94172003+PaullGit@users.noreply.github.com>
2022-12-29 14:13:42 +0100

    commit

--- viscon frontend/visconfrontend/src/components/CaseBox.tsx
+++ viscon frontend/visconfrontend/src/components/CaseBox.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import './CaseBox.css';
 import Ticket from './Ticket';
+import { GetTicketByAdminId} from "../services/TicketServices";
 
 
 function CaseBox(props) {
+    // hier wil ik iets doen
+    const [tickets, setTickets] = useState<any>([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            setTickets(await GetTicketByAdminId());
+        }
+        fetchData();
+        console.log();
+    }, []);
+
     return (
-    <div className = "title-box">
-        <div className="h-vertical-menu">
-        <Link to = '#' className="active">{props.name}</Link>
+        <div className="title-box">
+            <div className="h-vertical-menu">
+                <Link to='#' className="active">{props.name}</Link>
+            </div>
+            <div className="vertical-menu">
+                <Ticket name="1" />
+            </div>
         </div>
-        <div className="vertical-menu">
-            <Ticket name = "1"/>
-        </div>
-    </div>
 
     );
 
