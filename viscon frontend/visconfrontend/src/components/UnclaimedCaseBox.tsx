101890ac93287c4190c09befdae5a3f05ca7d6b9
PaullGit <94172003+PaullGit@users.noreply.github.com>
2022-12-29 14:13:42 +0100

    commit

--- viscon frontend/visconfrontend/src/components/UnclaimedCaseBox.tsx
+++ viscon frontend/visconfrontend/src/components/UnclaimedCaseBox.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {useEffect, useState} from 'react';
 import './CaseBox.css';
 import Ticket from './Ticket';
+import { GetTicketByAdminId} from "../services/TicketServices";
+
 
 
 function UnclaimedCaseBox(props) {
+    // hier wil ik iets doen
+    const [tickets, setTickets] = useState<any>([]);
+
+      useEffect(() => {
+    async function fetchData() {
+      setTickets(await GetTicketByAdminId());
+    }
+    fetchData();
+    console.log();
+  }, []);
+
     return (
     <div className = "title-box">
         <div className="h-vertical-menu">
