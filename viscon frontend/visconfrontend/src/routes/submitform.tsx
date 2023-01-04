e1356067efda096da85c2428628533ab1cd73386
KevinGit9 <1037132@hr.nl>
2023-01-04 15:03:36 +0100

    if user is not a keyuser they wont be able to create a ticket

--- viscon frontend/visconfrontend/src/routes/submitform.tsx
+++ viscon frontend/visconfrontend/src/routes/submitform.tsx
@@ -1,51 +1,63 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { getUserId } from '../services/LocalStorageManager';
+import { getUserId, getUserRole } from '../services/LocalStorageManager';
 import { CreateTicket } from '../services/TicketServices';
 
 import './submitform.css';
 
 
 function Submitform() {
-    //TODO: 
-    //Use the user input to create a ticket.
-    //Add a Component(because it will also get used in other screens) called "ConfirmScreen" which is a pop-up screen with a "continue" and "go back" button. This gets called when the User clicks on the Submit button.
-    //Add field in the Ticket Model which stores photos/videos.
-    //Add an optional field in which the User can add their phone number.
     let navigate = useNavigate();
-    const location = useLocation();
-    const queryParams = new URLSearchParams(location.search);
-    const machineId = queryParams.get('machineId');
-    const userId = getUserId();
-    console.log(`userId = ${userId}`);
-    console.log(`machineId = ${machineId}`);
+    if (getUserRole() == "user") return(NoPermission());
+    else return(Submit());
 
-    const handleTicketSubmit = () => {
-        CreateTicket(userId, machineId, ["this is", "from the", "submit form"]);
-        navigate("/usermenu");
+    function Submit() {
+        //TODO: 
+        //Use the user input to create a ticket.
+        //Add a Component(because it will also get used in other screens) called "ConfirmScreen" which is a pop-up screen with a "continue" and "go back" button. This gets called when the User clicks on the Submit button.
+        //Add field in the Ticket Model which stores photos/videos.
+        //Add an optional field in which the User can add their phone number.
+        const location = useLocation();
+        const queryParams = new URLSearchParams(location.search);
+        const machineId = queryParams.get('machineId');
+        const userId = getUserId();
+        console.log(`userId = ${userId}`);
+        console.log(`machineId = ${machineId}`);
+
+        const handleTicketSubmit = () => {
+            CreateTicket(userId, machineId, ["this is", "from the", "submit form"]);
+            navigate("/usermenu");
+        }
+
+        return (
+            <div className="submissionform">
+                <h1>Problem submission form</h1>
+                <form id="problemform">
+
+                    <p>Please describe the behaviour you're expecting.</p>
+                    <textarea className = "textbox"> </textarea> <br/>
+
+                    <p>Please describe the behaviour you're seeing.</p>
+                    <textarea className="textbox"> </textarea> <br />
+
+                    <p>Please enter any additional information.</p>
+                    <textarea className="textbox"> </textarea> <br />
+                    <input className="paperclip" type="file" multiple accept="image/*, video/*" ></input>
+                    <input type="submit" onClick={handleTicketSubmit}></input>
+                </form>
+            </div>
+        );
     }
 
-    return (
-        <div className="submissionform">
-            <h1>Problem submission form</h1>
-            <form id="problemform">
-
-                <p>Please describe the behaviour you're expecting.</p>
-                <textarea className = "textbox"> </textarea> <br/>
-
-                <p>Please describe the behaviour you're seeing.</p>
-                <textarea className="textbox"> </textarea> <br />
-
-                <p>Please enter any additional information.</p>
-                <textarea className="textbox"> </textarea> <br />
-                <input className="paperclip" type="file" multiple accept="image/*, video/*" ></input>
-                <input type="submit" onClick={handleTicketSubmit}></input>
-            </form>
-
-        </div>
-
-
-    );
+    function NoPermission() {
+        return (
+            <div>
+                <header> No Permission </header>
+                <p> Please contact a higher up in the company about the problem. </p>
+                <button onClick={() => navigate("/usermenu")}> Go Back to Menu. </button>
+            </div>
+        )
+    }
 }
 
 export default Submitform;
