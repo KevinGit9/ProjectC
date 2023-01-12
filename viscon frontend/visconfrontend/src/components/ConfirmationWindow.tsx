7b06a50e61c302799b5d36fe744771cb88ad8a19
KevinGit9 <1037132@hr.nl>
2023-01-12 01:02:50 +0100

    added pop up to confirm ticket submission + css

--- viscon frontend/visconfrontend/src/components/ConfirmationWindow.tsx
+++ viscon frontend/visconfrontend/src/components/ConfirmationWindow.tsx
@@ -3,9 +3,17 @@ import "./ConfirmationWindow.css";
 function ConfirmationWindow(props) {
     return (props.open) ? (
         <div className="dark">
-            <div className="confirmationWindowPanel">
-                <span className="close" onClick={() => props.setOpen(false)}> &times; </span>
-                <p> {props.text} </p>
+            <div className="confirmationWindow">
+                <div className="confirmationWindowHeader">
+                    <span className="close" onClick={() => props.setOpen(false)}> &times; </span>
+                </div>
+                <div className="confirmationWindowPanel">
+                    <p> {props.text} </p>
+                </div>
+                <div className="confirmationWindowButtons">
+                        <button onClick={() => props.setOpen(false)}> Back </button>
+                        <button onClick={props.continueButton}> Yes </button>
+                </div>
             </div>
         </div>
     ) : null;
