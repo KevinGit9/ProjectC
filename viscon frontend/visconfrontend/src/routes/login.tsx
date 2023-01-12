09280f1078702ee03cd481ec2fd8d63e0a3751c2
Rayanmrf <r.elmaaroufi@hotmail.com>
2023-01-12 14:36:34 -0200

    login fix

--- viscon frontend/visconfrontend/src/routes/login.tsx
+++ viscon frontend/visconfrontend/src/routes/login.tsx
@@ -1,45 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.css';
 import { useNavigate } from 'react-router';
 import { Login } from '../services/UserServices';
 import { getUserRole } from '../services/LocalStorageManager';
 
+
 const LoginLayout = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   let navigate = useNavigate();
-  const handleLogin = async (email: string, password: string) => {
+
+  const handleLogin = async () => {
     await Login(email, password);
     if (getUserRole() == "admin") navigate("/admin");
+   
     else navigate("/usermenu");
+    console.log(email);
   }
 
   return (
-      <div className="login-layout">
-          <h1>Login</h1>
-          <form>
-              <div className="form-group">
-                  <label>Email</label>
-                  <input type="email" name="email" />
-              </div>
-              <div className="form-group">
-                  <label>Password</label>
-                  <input type="password" name="password" />
-              </div>
-              <div className="form-group">
-                  <a href="#">Forget password?</a>
-              </div>
-              <button type="submit">Login</button>
-              <button type="button" className="back-button">Back</button>
-          </form>
-          <button onClick={() => handleLogin("visconadmin", "visconadmin")}> Temporary Log in button</button>
-      </div>
+    <div className="login-layout">
+      <h1>Login</h1>
+      <form>
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <a href="#">Forget password?</a>
+        </div>
+        <button onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
   );
 };
 
 export default LoginLayout;
-
-
-
-
 
 
 
