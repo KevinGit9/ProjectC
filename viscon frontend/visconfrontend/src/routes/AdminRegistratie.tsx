d3f57f091ea4d82f35e75794db0cbda236cbe050
Rayanmrf <r.elmaaroufi@hotmail.com>
2023-01-14 16:06:31 -0200

    registratiescherm met opgeslagen input

--- viscon frontend/visconfrontend/src/routes/AdminRegistratie.tsx
+++ viscon frontend/visconfrontend/src/routes/AdminRegistratie.tsx
@@ -1,32 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AdminRegistratie.css';
+
 const AdminRegistration = () => {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Name:', name);
+    console.log('Last Name:', lastName);
+    console.log('Email:', email);
+    console.log('Password:', password);
+    console.log('Confirm Password:', confirmPassword);
+  }
+
   return (
     <div className="registration-layout">
       <div className="reg-title">
         <h1>Create an Account</h1>
       </div>
       <div className="reg-form">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>
             Name:
-            <input type="text" placeholder="Enter your name" />
+            <input type="text" placeholder="Enter your name" value={name} onChange={e => setName(e.target.value)}/>
           </label>
           <label>
             Last Name:
-            <input type="text" placeholder="Enter your last name" />
+            <input type="text" placeholder="Enter your last name" value={lastName} onChange={e => setLastName(e.target.value)}/>
           </label>
           <label>
             Email:
-            <input type="email" placeholder="Enter your email" />
+            <input type="email" placeholder="Enter your email" value={email} onChange={e => setEmail(e.target.value)}/>
           </label>
           <label>
             Password:
-            <input type="password" placeholder="Enter password" />
+            <input type="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.target.value)}/>
           </label>
           <label>
             Confirm Password:
-            <input type="password" placeholder="Confirm password" />
+            <input type="password" placeholder="Confirm password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)}/>
           </label>
           <button type="submit">Register</button>
         </form>
@@ -34,7 +50,5 @@ const AdminRegistration = () => {
     </div>
   );
 };
+
 export default AdminRegistration;
-
-
-
