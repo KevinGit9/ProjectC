177e499508b13e506fa4586c1554b4cb667d4f0c
KevinGit9 <1037132@hr.nl>
2023-01-18 19:15:10 +0100

    mobile styling

--- viscon frontend/visconfrontend/src/routes/Problems.tsx
+++ viscon frontend/visconfrontend/src/routes/Problems.tsx
@@ -22,7 +22,6 @@ function Problems() {
       setProblems(await GetProblemsFromMachine(`${machine.id}/${problemType}`));
     }
     fetchData();
-    console.log(problemType);
   }, []);
 
   return (
@@ -40,12 +39,3 @@ function Problems() {
 }
 
 export default Problems;
-
-
-/*
-<Accordion buttonText="Product verkeerd op machine" panelText="Fotocellen controleren. Eventueel product handmatig op juiste positie plaatsen."/>
-<Accordion buttonText="Laden/lossen duurt te lang" panelText="Waarschijnlijk staat de pallet klem. Zorg dat deze weer goed staat en reset de baan."/>
-<Accordion buttonText="Niet veilig om te bewegen" panelText="De pallet steekt uit op de shuttle of een andere pallet op een baan aan het shuttle pad. Draai de pallet handmatig op de juiste positie."/>
-<Accordion buttonText="Geen verbinding met satelliet" panelText="Controleren of satelliet aan staat. Als deze in het kanaal staat een opgeladen moet worden, opladen met oplaadkabel en in handbediening terugzetten op de shuttle."/>
-<Accordion buttonText="Satelliet verkeer op machine" panelText="Fotocellen controleren. Eventueel product handmatig op juiste positie plaatsen."/>
-*/
